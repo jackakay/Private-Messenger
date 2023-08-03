f5fda6b92e1d9fbc1d917c04bb54d6c71fca74db
Jack <71041042+jackakay@users.noreply.github.com>
2023-08-03 16:04:33 +0100

    getFriends func

---
+++ Server/Server/Server/Controllers/GetFriendsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetFriendsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetFriends(JObject payload)
+        {
+            bool loggedIn = false;
+            loginClass newlogin = new loginClass();
+            newlogin = JsonConvert.DeserializeObject<loginClass>(payload.ToString());
+            foreach (User user in Globals.db.root.users)
+            {
+                if (newlogin.username == user.user && newlogin.password == user.password)
+                {
+                    string json = JsonConvert.SerializeObject(user.friends);
+                    return Ok(json);
+                    loggedIn = true;
+
+                }
+
+            }
+            if(!loggedIn)
+            {
+                return Ok("Fail");
+            }
+            else
+            {
+                return Ok("");
+            }
+            
+        }
+    }
+}
